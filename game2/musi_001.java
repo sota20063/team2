ab620d70f7fd3627e817b62e268daf4149d30ed3
sota20063 <20063@hakodate.kosen-ac.jp>
2024-12-16 13:20:53 +0900

    gitignore 更新~

--- game2/musi_001.java
+++ game2/musi_001.java
@@ -14,18 +14,12 @@ public class musi_001 extends Actor
      */
     public void act() 
     {
-<<<<<<< HEAD
+
         int x = getX();
         int y = getY();
         if( Greenfoot.isKeyDown( "w" ) ){
             setLocation( x,y-4 );
-=======
 
-        if(Greenfoot.isKeyDown("w")) 
-        {
-            setRotation(270);
-            move(4);
->>>>>>> 8d575da3d1a0475cec9cebe8ee231ad44345c69e
         }
         if( Greenfoot.isKeyDown( "a" ) ){
             setLocation( x-4,y );
